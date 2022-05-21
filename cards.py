02508c0949bfe76f3c428f4d7e4b55f9e87c511e
codeinchicago <vancewcole@gmail.com>
2022-05-21 08:39:27 -0500

    Adding players

--- cards.py
+++ cards.py
@@ -7,13 +7,14 @@ This project will be completed individually, but feel free to share ideas with y
 Once completed, commit the project to github and submit the link to this assignment.
 """
 
-
 #To do list
 # X Make Cards
-#Implement drawing cards, adding up their value. Be careful of Ace, can be 1 or 11.
+# X Implement drawing cards, adding up their value. 
+#Be careful of Ace, can be 1 or 11.
 #   Need to work on order: deck must be first shuffled, then card drawn, then card evaluated.
 #Implement busting, winning, adding player totals
 #Add player and humans
+#For implementation, shuffle deck before anything else.
 
 #Thanks to Brian Stanton for code on the deck.
 #Thanks to Stack Overflow for how to take a variable from one class into another.
@@ -21,43 +22,104 @@ Once completed, commit the project to github and submit the link to this assignm
 import random
 
 class Card:
+
     def __init__(self):
-        self.card = "Ace of Spades"
+        # self.card = "Ace of Spades"
         self.deck = []
     def shuffle_deck(self):
         suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
         values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
         self.deck = [(value, suit) for value in values for suit in suits]
         random.shuffle(self.deck)
-        #print(self.deck)
+        # print(self.deck)
         return self.deck
+    
     def draw_card(self):
         self.draw = self.deck.pop()
-        print(f'Your card is the {self.draw[0]} of {self.draw[1]}.')
+        #print(f'Your card is the {self.draw[0]} of {self.draw[1]}.')
         #print(type(draw))
         return self.draw
-    def card_value(self):
-        evaluated = self.draw_card()
-        if evaluated[0] in {'Jack', 'Queen', 'King'}:
-            value = 10
-        elif evaluated[0] == "Ace":
-            value = 11
-        else:
-            value = int(evaluated[0])
-        print(value)
-        return value
+
 
 
 class Human(Card):
-
+    def __init__(self):
+        self.hand = []
+        self.shuffled = False
+    def get_deck(self):
+        print(self.deck)
+        return self.deck
 
     def grab_card(self):
-        #print(self.card)
-        self.shuffle_deck()
-        print(self.deck)
-        self.draw_card()
+        #print('At start: ', self.shuffled)
+        if self.shuffled == False:
+            self.shuffle_deck()
+            self.shuffled = True
+        #print(self.deck)
+        newcard = self.draw_card()
+        self.hand.append(newcard)
+        #print(self.hand)
+        #print(self.hand[0][0])
+        #print('At end: ', self.shuffled)
+
+    def show_hand(self):
+        print(self.hand)
+    
+    #Need to loop through the first item of every item in the list, adding up their totals.
+    def card_value(self):
+        self.value = 0
+        for i in range(len(self.hand)):
+            if self.hand[i][0] in {'Jack', 'Queen', 'King'}:
+                self.value += 10
+            elif self.hand[i][0] == "Ace":
+                self.value += 11
+            else:
+                self.value += int(self.hand[i][0])
+            print(self.value)
+        #Busting
+        if self.value > 21:
+            print("You bust.")
+            bust = True
+        return self.value
+
+#Make two players, compare their values.
+
+def gamble():
+    pass
+#Initialize players
+
+#Get them cards
+
+#Compare a winner
+
+# class Dealer(Human):
+#     def __init__(self):
+#         self.hand = []
+#         self.shuffled = False
+    
+#     def reveal_card(self):
+#         self.grab_card()
+
+        
     
 my_card = Card()
 #print(my_card)
+# my_card.get_deck()
+# my_card.shuffle_deck()
+# my_card.get_deck()
 my_human = Human()
-my_human.grab_card()
+# my_human.grab_card()
+# my_human.grab_card()
+# my_human.card_value()
+#my_human.get_deck()
+
+# my_dealer = Dealer()
+# my_dealer.reveal_card()
+# my_dealer.show_hand()
+
+#my_human.grab_card()
+
+# my_human.card_value(my_human.grab_card())
+#a = my_human.grab_card()
+#print(a)
+#print(type(a))
